7b59187c8e8abe3d7f65dd664ac48651b59cf199
RealHenryMartinez <henrymtz0390@gmail.com>
2022-11-14 22:07:54 -0800

    finished an image uploader to backend route - still need to add more routes

--- src/pages/PostingPokemonPage.js
+++ src/pages/PostingPokemonPage.js
@@ -1,31 +1,75 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
+import axios from 'axios';
 
 const PostingPokemonPage = () => {
-  const [selectedImage, setSelectedImage] = useState(null);
+    const [newUser, setNewUser] = useState(
+        {
+            username: '',
+            pokemonName: '',
+            photo: '',
+        }
+    );
 
-  return (
-    <div>
-      <h1>Upload and Display Image usign React Hook's</h1>
-      {selectedImage && (
-        <div>
-        <img alt="not fount" width={"250px"} src={URL.createObjectURL(selectedImage)} />
-        <br />
-        <button onClick={()=>setSelectedImage(null)}>Remove</button>
-        </div>
-      )}
-      <br />
-     
-      <br /> 
-      <input
-        type="file"
-        name="myImage"
-        onChange={(event) => {
-          console.log(event.target.files[0]);
-          setSelectedImage(event.target.files[0]);
-        }}
-      />
-    </div>
-  );
-};
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const formData = new FormData();
+        formData.append('photo', newUser.photo);
+        formData.append('pokemonName', newUser.pokemonName);
+        formData.append('username', newUser.username);
+
+
+        // putting the form data appended to the new data retrieved and sent to the server
+        axios.post('http://localhost:4020/addPokemon', formData)
+             .then(res => {
+                console.log(res);
+             })
+             .catch(err => {
+                console.log(err);
+             });
+    }
+
+    const handleChange = (e) => {
+        setNewUser({...newUser, username: e.target.value} );
+    }
+
+    const pokemonNameChange = (e) => {
+      setNewUser({...newUser, pokemonName: e.target.value} );
+
+     }
+// it passes through the event and through the array, it will find the new item in the array of files 
+    const handlePhoto = (e) => {
+        setNewUser({...newUser, photo: e.target.files[0]});
+    }
+
+    return (
+        <form onSubmit={handleSubmit} encType='multipart/form-data'>
+            <input 
+                type="file" 
+                accept=".png, .jpg, .jpeg"
+                name="photo"
+                onChange={handlePhoto}
+            />
+
+            <input 
+                type="text"
+                placeholder="name"
+                name="username"
+                value={newUser.username}
+                onChange={handleChange}
+            />
+
+            <input 
+                type="text"
+                name="pokemonName"
+                value={newUser.pokemonName}
+                onChange={pokemonNameChange}
+            />
+
+            <input 
+                type="submit"
+            />
+        </form>
+    );
+}
 
 export default PostingPokemonPage;
